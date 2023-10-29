6e9f93a295f49aced2b71b4a26185ad28031ca9e
Mohit Kumar Dubey <mohitdb7@gmail.com>
2023-10-29 23:32:33 +0530

    changes to add new project and golang readMe file

---
+++ 02-banking-hexagonal-architecture/banking-functionality/app/appError.go
@@ -0,0 +1,44 @@
+package app
+
+import (
+	"net/http"
+)
+
+type AppError struct {
+	Code    int    `json:"code, omitempty"`
+	Message string `json:"message"`
+}
+
+func NoDataFoundAppError(message string) *AppError {
+	return &AppError{
+		Code:    http.StatusNotFound,
+		Message: message,
+	}
+}
+
+func InternalIssueAppError(message string) *AppError {
+	return &AppError{
+		Code:    http.StatusInternalServerError,
+		Message: message,
+	}
+}
+
+func IssueInInsert(message string) *AppError {
+	return &AppError{
+		Code:    http.StatusUnprocessableEntity,
+		Message: message,
+	}
+}
+
+func UnauthorisedUser(message string) *AppError {
+	return &AppError{
+		Code:    http.StatusUnauthorized,
+		Message: message,
+	}
+}
+
+func (s AppError) ErrorMessage() *AppError {
+	appError := AppError{Message: s.Message}
+
+	return &appError
+}
